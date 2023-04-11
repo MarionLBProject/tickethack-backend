f17a5a5496e7ca609e8948ca2ae801dd7546192b
Marion <marionlaurin@hotmail.com>
2023-04-11 18:23:12 +0200

    backend1erroute

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = process.env.CONNECTION_STRING 
+const connectionString = "mongodb+srv://MarionLB:B1gjBYL8qdipSX9f@cluster0.j4uxon5.mongodb.net/tickethack"
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 }) 
 .then(() => console.log("Database connected"))
