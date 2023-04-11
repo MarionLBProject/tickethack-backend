8ca946e39e58c37c77b1c671e406a8a2a4997543
Marion <marionlaurin@hotmail.com>
2023-04-11 11:44:19 +0200

    connection backend

---
+++ models/trips.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose")
+
+const tripsSchema = mongoose.Schema({ 
+	departure: String,
+    destination: String,
+    date: Date,
+})
+
+const Trip = mongoose.model("trips", tripsSchema) 
+
+module.exports = Trip
