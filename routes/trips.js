4bc041ac770ffcceee1fd5d01d45e10bf9e77b25
Hugo <hugolevisse1@gmail.com>
2023-04-12 12:27:58 +0200

    package

--- routes/trips.js
+++ routes/trips.js
@@ -5,32 +5,47 @@ const fetch = require("node-fetch");
 
 var Trip = require('../models/trips') 
 
-router.get('/', (req, res) => {
-  // if (!req.body.departure || !req.body.arrival || !req.body.date) {
-    console.log(req.body.departure)
-    const date = date.format('L');
-    console.log(req.body.date)
 
-  //   res.json({ result: false, error: 'Please fill the fields!'})
-  //   return;
-  // }
-  // Trip.find({
-  //   $and: [
-  //     { departure : req.body.departure },
-  //     { arrival : req.body.arrival },
-  //     { date: req.body.date },
-  //   ]
-  // })
+router.post('/', (req,res) => {
+  if(!req.body.departure || !req.body.arrival || !req.body.date){
+    res.json({result: false, error: 'Empty fields'})
+    return;
+  }
+  Trip.find({
+  { departure : req.body.departure },
+  { arrival : req.body.arrival },
+            
+  }).then(data => {console.log(data)})
 })
+
+
+
+// router.get('/', (req, res) => {
+//   // if (!req.body.departure || !req.body.arrival || !req.body.date) {
+//     console.log(req.body.departure)
+//     const date = date.format('L');
+//     console.log(req.body.date)
+
+//   //   res.json({ result: false, error: 'Please fill the fields!'})
+//   //   return;
+//   // }
+//   // Trip.find({
+//   //   $and: [
+//   //     { departure : req.body.departure },
+//   //     { arrival : req.body.arrival },
+//   //     { date: req.body.date },
+//   //   ]
+//   // })
+// })
 
 //route get trip sur le search
 
-router.delete('/:id', function(req, res, next) {
-  console.log(req.params.id);
+// router.delete('/:id', function(req, res, next) {
+//   console.log(req.params.id);
 
-  // mise en place de la suppresion
+//   // mise en place de la suppresion
 
 
-  res.json({});
-});
+//   res.json({});
+// });
 module.exports = router;
